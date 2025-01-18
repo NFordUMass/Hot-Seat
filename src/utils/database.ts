cc302c2e34c22ac190395df043eae94b82991f13
Noah F <nhford@umass.edu>
2025-01-18 10:43:07 -0500

    skeleton current table

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,3 +5,7 @@ const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL
 const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
 
 export const supabase = supabaseUrl && supabaseKey ? createClient<Database>(supabaseUrl, supabaseKey) : null;
+
+export function imgPath(sport:string,abbrev:string,year=2024){
+    return `/images/${sport}/${abbrev.toLowerCase()}-${year}.png`;
+}
