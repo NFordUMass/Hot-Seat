09c9b06074708dfb3345cc4156c6437be2f7a64b
Noah F <nhford@umass.edu>
2025-01-22 12:03:14 -0500

    colorful table

--- src/components/Heat_Table.tsx
+++ src/components/Heat_Table.tsx
@@ -4,12 +4,13 @@ import { imgPath } from "../utils/database.ts";
 import CurrentRow from "./Row.tsx";
 
 interface Props {
+  year: number;
   source: Tables<"heat_index">[];
 }
 
-type sortkey = "name" | "team" | "prob";
+type sortkey = "name" | "team" | "prob" | "fired";
 
-export default function Heat_Table({ source }: Props) {
+export default function Heat_Table({ year, source }: Props) {
   const [coaches, setCoaches] = useState<Tables<"heat_index">[]>(source);
   const [sorted, setSorted] = useState({ key: "prob", dir: "asc" });
 
@@ -24,18 +25,30 @@ export default function Heat_Table({ source }: Props) {
   }
 
   return (
-    <table className="w-full text-left">
+    <table className="w-full text-left bg-white text-black">
       <thead>
-        <tr className="font-mono text-base border-t border-b">
-          <th onClick={() => handleSort("team")}>Team</th>
-          <th onClick={() => handleSort("name")}>Coach</th>
-          <th onClick={() => handleSort("prob")}>Heat Index</th>
+        <tr className="font-mono text-sm md:text-base lg:text-xl border-t border-b">
+          {[
+            { key: "team", label: "Team" },
+            { key: "name", label: "Coach" },
+            { key: "prob", label: "Heat Index" },
+            { key: "fired", label: "Outcome" },
+          ].map((row) => (
+            <th
+              className="px-0.5 "
+              onClick={() => handleSort(row.key as sortkey)}
+            >
+              {row.label}
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {coaches?.map((row, index) => (
-          <CurrentRow key={`${row.id}_${row.year}`} current={row} />
-        ))}
+        {coaches
+          ?.filter((row) => row.year == year)
+          .map((row, index) => (
+            <CurrentRow key={`${row.id}_${row.year}`} current={row} />
+          ))}
       </tbody>
     </table>
   );
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -7,16 +7,26 @@ interface Props {
 
 const CurrentRow: React.FC<Props> = ({ current }) => {
   return (
-    <tr className="border-b border-neutral-600">
-      <td>
+    <tr
+      className="border-b border-neutral-600"
+      style={{ backgroundColor: current.fired ? "red" : "inherit" }}
+    >
+      <td className="px-0.5">
         <img
           src={imgPath("nfl", current.team, 2024)}
-          className="w-16"
+          className="w-8 lg:w-16"
           alt={current.team}
         />
       </td>
-      <td className="text-xl font-bold">{current.name}</td>
-      <td className="text-xl">{current.prob.toFixed(2)}</td>
+      <td className="text-sm md:text-base lg:text-xl font-bold">
+        {current.name}
+      </td>
+      <td className="text-sm md:text-base lg:text-xl">
+        {current.prob.toFixed(2)}
+      </td>
+      <td className="text-sm md:text-base lg:text-xl">
+        {current.fired ? "Fired" : "Safe"}
+      </td>
     </tr>
   );
 };
