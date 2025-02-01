32ac48b2d3c19a8855d3749e8e720be53cc3746e
Noah F <nhford@umass.edu>
2025-02-01 16:07:02 -0500

    operational hardcoded api call

--- src/components/WhatIf.tsx
+++ src/components/WhatIf.tsx
@@ -9,6 +9,44 @@ interface Props {
 export default function WhatIf({ seasonRow }: Props) {
   const [record, setRecord] = useState(5);
   const [round, setRound] = useState(2);
+  const [result, setResult] = useState([1, 0]);
+
+  const [inputs, setInputs] = useState({
+    feature1: 3,
+    feature2: 2.0,
+    feature3: 4,
+    feature4: 7,
+    feature5: 4,
+    feature6: 0.5,
+    feature7: -3,
+    feature8: 2,
+    feature9: 0.2,
+    feature10: 2.5,
+    feature11: 1,
+    feature12: 1,
+    feature13: 0.2,
+    feature14: 1,
+    feature15: 0.307,
+    feature16: 0.19,
+    feature17: 0.307,
+    feature18: 0,
+    feature19: 0,
+    feature20: 0,
+  });
+
+  const handleClick = () => {
+    const featureArray = Object.values(inputs); // Create array of features
+
+    fetch("https://hot-seat-backend.onrender.com/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ features: featureArray }),
+    })
+      .then((response) => response.json())
+      .then((response) => setResult(response["prediction"][0]));
+  };
 
   return seasonRow ? (
     <div>
@@ -46,6 +84,8 @@ export default function WhatIf({ seasonRow }: Props) {
       </span>
       {/* Engine */}
       {/* Call Model */}
+      <button onClick={handleClick}>Run Engine</button>
+      {`Predicted Heat Index: ${result[0]}`}
       {/* Take features as input */}
       {/* Have precomputed suggested feature values */}
     </div>
